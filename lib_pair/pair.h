5fc2ab3c1c63f2522609701e3c27baf0c70ac2f0
dma203 <matvey.demidovich@yandex.ru>
2024-09-05 11:52:13 +0300

    minor correction

--- lib_pair/pair.h
+++ lib_pair/pair.h
@@ -1,8 +1,8 @@
 // Copyright 2024 Matvey Demidovich
 
-#ifndef LIB_EASY_EXAMPLE_EASY_EXAMPLE_H_
-#define LIB_EASY_EXAMPLE_EASY_EXAMPLE_H_
+#ifndef LIB_PAIR_PAIR_H_
+#define LIB_PAIR_PAIR_H_
 
 float division(int a, int b);
 
-#endif  // LIB_EASY_EXAMPLE_EASY_EXAMPLE_H_
+#endif  // LIB_PAIR_PAIR_H_
