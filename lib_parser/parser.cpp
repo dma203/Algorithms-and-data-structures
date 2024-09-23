9d4184f5bc47190442694bef8d28556fc9e154e9
dma203 <matvey.demidovich@yandex.ru>
2024-09-23 17:36:08 +0300

    lib_parser added

---
+++ lib_parser/parser.cpp
@@ -0,0 +1,40 @@
+// Copyright 2024 Matvey Demidovich
+
+#include "../lib_parser/parser.h"
+#include "../lib_stack/stack.h"
+#include <string>
+#include <stdexcept>
+
+bool _is_correct(const std::string exp){
+
+	TStack<char> stack(exp.length());
+	char bracket;
+
+	for (size_t i = 0; i < exp.length(); i++) {
+		if (exp[i] == '(' || exp[i] == '{' || exp[i] == '[') {
+			stack.push(exp[i]);
+		}
+		else if (exp[i] == ')' || exp[i] == '}' || exp[i] == ']') {
+			if (stack.isEmpty()) {
+				throw std::logic_error("Missing opening bracket");
+				return false;
+			}
+			bracket = stack.top();
+			if (bracket != exp[i]) {
+				throw std::logic_error("Missing closing bracket");
+				return false;
+			}
+		}
+		else if (exp[i] < '0' || '9' < exp[i] && exp[i] < 'A' || 'Z' < exp[i]
+			&& exp[i] < 'a' || 'z' < exp[i] && exp[i] != '*' && exp[i] != '/'
+			&& exp[i] != '+' && exp[i] != '-') {
+			throw std::logic_error("Unexpect token");
+			return false;
+		}
+		else if (stack.isEmpty()) {
+			throw std::logic_error("Expression is missing");
+			return false;
+		}
+		else return true;
+	}
+}
